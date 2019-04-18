c7f79caf194a686cc449d17bc96472bea6ec7756
Robin SOLDE <robin.solde@gmail.com>
2019-04-18 14:56:28 +0200

    test creneaux

--- RecrutementTest/UnitTest1.cs
+++ RecrutementTest/UnitTest1.cs
@@ -1,20 +1,37 @@
 using ddd;
 using NUnit.Framework;
+using System;
 
 namespace Tests
 {
     public class Tests
     {
+        public Creneau creneau1;
+        public Creneau creneau2;
         [SetUp]
         public void Setup()
         {
-            Creneau creneau;
+           // Creneau creneau1;
+            //Creneau creneau2;
         }
 
         [Test]
-        public void Test1()
+        public void assertTwoCreneauEqualsAreEquals()
         {
-            Assert.Pass();
+
+            DateTime dateTime = DateTime.Now;
+            var duration = 10;
+            creneau1 = new Creneau(dateTime, duration);
+            creneau2 = new Creneau(dateTime, duration);
+            Assert.True(creneau1.Equals(creneau2));
+        }
+        [Test]
+        public void assertTwoCreneauNotEqualsAreNotEquals()
+        {
+            var duration = 10;
+            creneau1 = new Creneau(DateTime.Now, duration);
+            creneau2 = new Creneau(DateTime.Now, duration);
+            Assert.False(creneau1.Equals(creneau2));
         }
     }
 }
