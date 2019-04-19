2f418319ac22c5fcbce8b9bae50fe10a74cee205
takethispie <takethispie@gmail.com>
2019-04-19 16:05:37 +0200

    added SalleRepository
    added PlanifierEntretien usecase

---
+++ Recrutement/Infrastructure/SalleRepository.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ddd
+{
+    public class SalleRepository : IRepository<Salle>
+    {
+        public IEnumerable<Salle> Collection => collection;
+        private List<Salle> collection;
+        
+        public bool Ajouter(Salle obj)
+        {
+            if (collection.Any(rec => rec.Equals(obj))) return false;
+            collection.Add(obj);
+            return true;
+        }
+
+        public bool Supprimer(Salle obj) => collection.Remove(obj);
+    }
+}
---
+++ Recrutement/UseCases/PlanifierEntretien.cs
@@ -0,0 +1,10 @@
+namespace ddd.UseCases
+{
+    public class PlanifierEntretien
+    {
+        public PlanifierEntretien(SalleRepository salleRepo, RecruteurRepository recruteurRepo)
+        {
+            
+        }
+    }
+}
