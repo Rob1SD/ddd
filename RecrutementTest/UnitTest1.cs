b232cbe5863daf3cb98e71517ef78ee0cc976b44
Robin SOLDE <robin.solde@gmail.com>
2019-04-19 14:21:15 +0200

    test recruteur

--- RecrutementTest/UnitTest1.cs
+++ RecrutementTest/UnitTest1.cs
@@ -1,6 +1,9 @@
 using ddd;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using System.Runtime;
+
 
 namespace Tests
 {
@@ -32,6 +35,39 @@ namespace Tests
             creneau1 = new Creneau(DateTime.Now, duration);
             creneau2 = new Creneau(DateTime.Now, duration);
             Assert.False(creneau1.Equals(creneau2));
+        }
+        [Test]
+        public void assertThatRecruteurCannotTestCandidatIfDifferentTech()
+        {
+            
+            Personne recruteurPersonne = new Personne("Antoine", "Sauvignet");
+            string[] recruteurCompetence = {".Net", "js"};
+            Profil recruteurProfil = new Profil(new List<string>(recruteurCompetence) , 5);
+            Recruteur recruteur = new Recruteur(recruteurPersonne, recruteurProfil);
+
+            Personne candidatPersonne = new Personne("Robin", "Soldé");
+            string[] candidatCompetence = {"C", "Ruby"};
+            Profil candidatProfil = new Profil(new List<string>(candidatCompetence), 5);
+            Candidat candidat = new Candidat(candidatPersonne, candidatProfil);
+
+            Assert.False(recruteur.PeutTester(candidat));
+        }
+
+        [Test]
+         public void assertThatRecruteurCannotTestCandidatIfNotMireExperienceThatCandidat()
+        {
+            
+            Personne recruteurPersonne = new Personne("Antoine", "Sauvignet");
+            string[] recruteurCompetence = {".Net", "js"};
+            Profil recruteurProfil = new Profil(new List<string>(recruteurCompetence) , 4);
+            Recruteur recruteur = new Recruteur(recruteurPersonne, recruteurProfil);
+
+            Personne candidatPersonne = new Personne("Robin", "Soldé");
+            string[] candidatCompetence = {".Net", "js"};
+            Profil candidatProfil = new Profil(new List<string>(candidatCompetence), 5);
+            Candidat candidat = new Candidat(candidatPersonne, candidatProfil);
+
+            Assert.False(recruteur.PeutTester(candidat));
         }
     }
 }
